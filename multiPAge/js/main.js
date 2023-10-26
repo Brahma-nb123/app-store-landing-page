2d03c0018b20d60aed27e5ee33d75f417c8a9dde
Brahmananda Behera <104623747+Brahma-nb123@users.noreply.github.com>
2023-10-26 18:44:27 +0530

    Add files via upload

---
+++ multiPAge/js/main.js
@@ -0,0 +1,20 @@
+alert("hi");
+// var element = document.getElementById("#llll").getAttribute("class");
+// console.log(element);
+
+// var kk = document.querySelector(".text-2");
+// var kk = document.querySelectorAll("#llll")[1];
+// console.log(kk);
+
+// var el;
+// el = document.getElementById("llll").innerHTML;
+// console.log(el);
+// var dd = document.URL;
+// console.log(dd);
+// var bb = document.getElementsByClassName("text-2")[1];
+// console.log(bb);
+
+
+var el;
+el = document.getElementById("llll").getAttributeNode("onclick");
+console.log(el); 
